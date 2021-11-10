fc95a6625ac12f9fbde2fe808f262a144faf2203
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-11-10 11:16:53 +0530

    Create test_trending.py

---
+++ tests/test_trending.py
@@ -0,0 +1,7 @@
+from py-tiktok import TikTokAPI
+from utils import read_json_from_file
+
+api = TikTokAPI(read_json_from_file("cookie.json"))
+retval = api.getTrending(count=5)
+print("Trending Videos")
+print(retval)
